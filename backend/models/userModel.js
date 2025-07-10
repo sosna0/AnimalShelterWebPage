38300a7103fd62ad06ce8761701707a038e96316
sosna0 <ksosnowski01@gmail.com>
2025-07-10 12:38:02 +0200

    Created User model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,6 +3,39 @@ const sequelize = require('../config/db');
 
 
 const User = sequalize.define('User', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+
+    password: {
+            type: DataTypes.STRING,
+            allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM('public', 'staff', 'admin'),
+        defaultValue: 'public'
+    },
+    
+//   phone: STRING // chyba zbędne
+//   created_at: TIMESTAMP // chyba zbędne
+//   updated_at: TIMESTAMP // chyba zbędne
 
 });
 
