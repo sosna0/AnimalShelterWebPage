8b9c10d87719877937fd26bcfa0fd8e9bbc74974
gregwojcik <grzegorz02.wojcik@student.uj.edu.pl>
2025-08-16 21:53:14 +0200

    Changed presets to match the changes in models

--- backend/config/presets/animalPreset.js
+++ backend/config/presets/animalPreset.js
@@ -1,11 +1,81 @@
 const animalPreset = [
-    { name: 'Perrito', species: 'Dog', age: 3, gender: 'Male', weight: 5.6, description: 'Therapy dog that will melt your heart with his smile.', adoptionStatus: 'Available', image: null },
-    { name: 'Rumble', species: 'Cat', age: 1, gender: 'Female', weight: 4.5, description: 'Playful kitten that always creates mess.', adoptionStatus: 'Available', image: null },
-    { name: 'Snowball', species: 'Dog', age: 5, gender: 'Female', weight: 6.0, description: 'Fluffy white dog that loves to play in the snow.', adoptionStatus: 'Available', image: null },
-    { name: 'Kiwi', species: 'Other', age: 5, gender: 'Unknown', weight: 2.0, description: 'A mysterious Kiwi bird. Not much is known about it, but it\'s definitely unique.', adoptionStatus: 'Unavailable', image: null },
-    { name: 'Burek', species: 'Dog', age: 12, gender: 'Male', weight: 6.35, description: 'Just an old, loud dog from your neighborhood, with a heart of gold.', adoptionStatus: 'Available', image: null },
-    { name: 'Lasty', species: 'Other', age: 99, gender: 'Female', weight: 15.9, description: 'An everlasting tortoise, Lasty has seen it all and is ready to share her wisdom.', adoptionStatus: 'Unavailable', image: null },
-    { name: 'Rocket', species: 'Cat', age: 2, gender: 'Male', weight: 4.86, description: 'Rocket is lively and energetic, can start a sprint in a matter of seconds.', adoptionStatus: 'Adopted', image: null }
+    {
+        name: 'Perrito',
+        species: 'Dog',
+        age: 3,
+        gender: 'Male',
+        weight: 5.6,
+        description: 'Therapy dog that will melt your heart with his smile.',
+        longDescription: 'Perrito is a joyful 3-year-old pup whose warm smile and gentle nature make him a perfect therapy dog. He loves cuddling, wagging his tail, and spreading happiness wherever he goes. Ideal for families or individuals seeking a loyal companion to brighten their days. Ready to share his love!',
+        adoptionStatus: 'Available',
+        image: null
+    },
+    {
+        name: 'Rumble',
+        species: 'Cat',
+        age: 1,
+        gender: 'Female',
+        weight: 4.5,
+        description: 'Playful kitten that always creates mess.',
+        longDescription: 'Rumble, a spirited 1-year-old kitten, is a whirlwind of fun! Her playful antics and boundless energy will keep you entertained, though she may leave a trail of toys in her wake. Perfect for an active home ready to embrace her mischievous charm and shower her with love and playtime!',
+        adoptionStatus: 'Available',
+        image: null
+    },
+    {
+        name: 'Snowball',
+        species: 'Dog',
+        age: 5,
+        gender: 'Female',
+        weight: 6.0,
+        description: 'Fluffy white dog that loves to play in the snow.',
+        longDescription: 'Snowball is a 5-year-old fluffy bundle of joy who thrives in frosty fun! Her love for romping in the snow and chasing snowflakes makes her a winter enthusiast’s dream. With her soft fur and playful spirit, she’s ready to bring warmth and excitement to a loving home all year round!',
+        adoptionStatus: 'Available',
+        image: null
+    },
+    {
+        name: 'Kiwi',
+        species: 'Other',
+        age: 5,
+        gender: 'Unknown',
+        weight: 2.0,
+        description: 'A mysterious Kiwi bird. Not much is known about it, but it\'s definitely unique.',
+        longDescription: 'Kiwi, a 5-year-old Kiwi bird, is a rare and enigmatic gem! This unique creature captivates with its quirky charm and elusive nature. Perfect for an experienced adopter fascinated by exotic animals, Kiwi promises to bring a touch of mystery and wonder to a specialized, caring home.',
+        adoptionStatus: 'Unavailable',
+        image: null
+    },
+    {
+        name: 'Burek',
+        species: 'Dog',
+        age: 12,
+        gender: 'Male',
+        weight: 6.35,
+        description: 'Just an old, loud dog from your neighborhood, with a heart of gold.',
+        longDescription: 'Burek, a 12-year-old neighborhood legend, may be loud, but his heart is pure gold! This senior dog loves leisurely strolls and cozy naps. Perfect for a calm home looking for a loyal friend to share quiet moments and endless love. Burek’s ready to be your faithful companion!',
+        adoptionStatus: 'Available',
+        image: null
+    },
+    {
+        name: 'Lasty',
+        species: 'Other',
+        age: 99,
+        gender: 'Female',
+        weight: 15.9,
+        description: 'An everlasting tortoise, Lasty has seen it all and is ready to share her wisdom.',
+        longDescription: 'Lasty, a wise 99-year-old tortoise, carries a lifetime of stories in her sturdy shell. This gentle giant moves slowly but loves deeply, offering calm companionship. Ideal for a patient home ready to cherish her timeless wisdom and provide a serene space for her enduring journey.',
+        adoptionStatus: 'Unavailable',
+        image: null
+    },
+    {
+        name: 'Rocket',
+        species: 'Cat',
+        age: 2,
+        gender: 'Male',
+        weight: 4.86,
+        description: 'Rocket is lively and energetic, can start a sprint in a matter of seconds.',
+        longDescription: 'Rocket, a 2-year-old feline dynamo, lives up to his name with lightning-fast sprints and boundless energy! This lively cat loves to play and explore, making him perfect for an active home ready to keep up with his zest for life. Adopt Rocket for endless fun and furry cuddles!',
+        adoptionStatus: 'Adopted',
+        image: null
+    }
 ];
 
 module.exports = animalPreset;
--- backend/config/presets/userPreset.js
+++ backend/config/presets/userPreset.js
@@ -1,6 +1,20 @@
 const userPreset = [
-    { name: 'Jan', surname: 'Kowalski', username: 'staff', email: 'staff@staff.com', password: '$2b$10$4185UECKEvSD0Z6gx8UJUeHNW8PKhfK5Id8IsBNYCUH7xnyC2skNS', role: 'staff' },
-    { name: 'Adam', surname: 'Niezgódka', username: 'user', email: 'user@user.com', password: '$2b$10$mDFtYTmQXNHkNDDc/tGcWOZqjuA123fVN4SKVMvQiwCnIAVg2Msxu', role: 'public' }
+    { 
+        name: 'Jan', 
+        surname: 'Kowalski', 
+        username: 'staff', 
+        email: 'staff@staff.com', 
+        password: '$2b$10$4185UECKEvSD0Z6gx8UJUeHNW8PKhfK5Id8IsBNYCUH7xnyC2skNS', 
+        role: 'staff' 
+    },
+    { 
+        name: 'Adam', 
+        surname: 'Niezgódka', 
+        username: 'user', 
+        email: 'user@user.com', 
+        password: '$2b$10$mDFtYTmQXNHkNDDc/tGcWOZqjuA123fVN4SKVMvQiwCnIAVg2Msxu', 
+        role: 'public' 
+    }
 ];
 
 module.exports = userPreset;
