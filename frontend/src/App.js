76759f5004a5d86dc33305fc0aab9fc5a7e03d28
gregwojcik <grzegorz02.wojcik@student.uj.edu.pl>
2025-08-19 17:24:05 +0200

    Fixed formatting of App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,73 +17,65 @@ import { ProtectedRoute } from './components/access/ProtectedRoute';
 import { AuthProvider } from './hooks/use-auth';
 
 function App() {
-  return (
-    <AuthProvider>
-      <Router>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/animals" element={<Animals />} />
-              <Route path="/animals/:id" element={<AnimalDetails />} />
-              <Route 
-                path="/animals/new" 
-                element={
-                  <ProtectedRoute allowedRoles={['staff']}>
-                    <AnimalNew />
-                  </ProtectedRoute>
-                }
-              />
-              <Route 
-                path="/animals/edit/:id" 
-                element={
-                  <ProtectedRoute allowedRoles={['staff']}>
-                    <AnimalEdit />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/donate" element={<Donations />} />
-              <Route path="/donate/payment" element={<DonationNew />} />
-              
-              <Route path="/" element={<ProfileNavigationCard />}>
-
-                <Route 
-                  path="/user-profile" 
-                  element={
-                    <ProtectedRoute allowedRoles={['public']}>
-                      <UserProfile />
-                    </ProtectedRoute>
-                  }
-                />
-                
-                <Route 
-                  path="/user-adoptions" 
-                  element={
-                    <ProtectedRoute allowedRoles={['public']}>
-                      <UserAdoptions />
-                    </ProtectedRoute>
-                  }
-                />
-                
-                <Route 
-                  path="/user-donations" 
-                  element={
-                    <ProtectedRoute allowedRoles={['public']}>
-                      <UserDonations />
-                    </ProtectedRoute>
-                  }
-                />
-              
-              </Route>
-
-
-              
-            </Routes>
-          </Layout>
-      </Router>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<Router>
+				<Layout>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/animals" element={<Animals />} />
+						<Route path="/animals/:id" element={<AnimalDetails />} />
+						<Route 
+							path="/animals/new" 
+							element={
+								<ProtectedRoute allowedRoles={['staff']}>
+									<AnimalNew />
+								</ProtectedRoute>
+							}
+						/>
+						<Route 
+							path="/animals/edit/:id" 
+							element={
+								<ProtectedRoute allowedRoles={['staff']}>
+									<AnimalEdit />
+								</ProtectedRoute>
+							}
+						/>
+						<Route path="/donate" element={<Donations />} />
+						<Route path="/donate/payment" element={<DonationNew />} />
+						<Route path="/" element={<ProfileNavigationCard />}>
+							<Route 
+								path="/user-profile" 
+								element={
+									<ProtectedRoute allowedRoles={['public']}>
+										<UserProfile />
+									</ProtectedRoute>
+								}
+							/>
+							<Route 
+								path="/user-adoptions" 
+								element={
+									<ProtectedRoute allowedRoles={['public']}>
+										<UserAdoptions />
+									</ProtectedRoute>
+								}
+							/>
+							<Route 
+								path="/user-donations" 
+								element={
+									<ProtectedRoute allowedRoles={['public']}>
+										<UserDonations />
+									</ProtectedRoute>
+								}
+							/>
+						</Route>
+					</Routes>
+				</Layout>
+			</Router>
+		</AuthProvider>
+	);
 }
 
 export default App;
