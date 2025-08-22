84a655f20f1505dbd556eb4043ec16ef93b80b6a
gregwojcik <grzegorz02.wojcik@student.uj.edu.pl>
2025-08-22 02:44:39 +0200

    Added service functions for adoptions

--- frontend/src/api/services/adoptionService.js
+++ frontend/src/api/services/adoptionService.js
@@ -11,7 +11,28 @@ export const getAdoptionById = async (id) => {
     return response.data;
 };
 
+export const getUserAdoptions = async (userId) => {
+    const response = await axios.get(endpoints.adoptions.getByUserId(userId));
+    return response.data;
+};
+
 export const createAdoption = async (data) => {
     const response = await axios.post(endpoints.adoptions.create, data);
+    return response.data;
+};
+
+export const updateAdoption = async (id, data) => {
+    const response = await axios.put(endpoints.adoptions.update(id), data);
+    return response.data;
+};
+
+export const updateAdoptionStatus = async (id, status, response) => {
+    const data = { status, response };
+    const apiResponse = await axios.put(endpoints.adoptions.updateStatus(id), data);
+    return apiResponse.data;
+};
+
+export const deleteAdoption = async (id) => {
+    const response = await axios.delete(endpoints.adoptions.delete(id));
     return response.data;
 };
