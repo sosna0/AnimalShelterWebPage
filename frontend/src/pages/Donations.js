b5dc7aa0c7aec502d393fefeea7f458c5b83e204
sosna0 <ksosnowski01@gmail.com>
2025-08-29 02:43:49 +0200

    Changed donation page for staff users

--- frontend/src/pages/Donations.js
+++ frontend/src/pages/Donations.js
@@ -4,12 +4,15 @@ import DonationCard from "../components/donations/DonationCard";
 import { getDonations } from "../api/services/donationService";
 import AchievementsCard from "../components/donations/AchievementsBox";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { RoleOnly } from "../components/access/RoleOnly";
+import { useAuth } from "../hooks/use-auth";
 
 const Donations = () => {
     const [donations, setDonation] = useState([]); // loaded donations state
     const [loading, setLoading] = useState(true); // loading state for spinner
     const [error, setError] = useState(null); // error state for error messages
     const [amountRaised, setAmountRaised] = useState(0); // total amount raised
+    const { user } = useAuth();
 
     useEffect(() => {
         const fetchDonations = async () => {
@@ -37,45 +40,53 @@ const Donations = () => {
 
     return (
         <Container className="container-fluid">
-            <Row className="px-4">
-                <Col xs={12} lg={7} className="mt-5 mb-4 mb-lg-5 h-100 align-items-stretch">
-                    <Container 
-                        className="py-4 px-4 mb-4 custom-container"
-                    >
-                        <h1 className="display-6 text-center fw-bold mt-3">
-                            Your Support 
-                            <br/>
-                            Makes a Difference
-                        </h1>
-                    
-                        <h4 className="lead mt-3 text-center">
-                            Every donation helps us rescue, care for,
-                            <br/>                            
-                            and find new home for animals in need. 
-                            <br/>                            
-                            Together, we can give them a second chance.
-                        </h4>
+            <Row className="px-4 justify-content-center">
+                <RoleOnly allowedRoles={['public', 'guest']}>    
+                    <Col xs={12} lg={7} className="mt-5 mb-4 mb-lg-5 h-100 align-items-stretch">
+                        <Container 
+                            className="py-4 px-4 mb-4 custom-container"
+                        >
+                            <h1 className="display-6 text-center fw-bold mt-3">
+                                Your Support 
+                                <br/>
+                                Makes a Difference
+                            </h1>
+                        
+                            <h4 className="lead mt-3 text-center">
+                                Every donation helps us rescue, care for,
+                                <br/>                            
+                                and find new home for animals in need. 
+                                <br/>                            
+                                Together, we can give them a second chance.
+                            </h4>
 
-                        <div className="text-center mt-4">
-                            <Button
-                                variant="primary"
-                                type="submit"
-                                className="btn btn-lg btn-primary custom-btn w-100 d-flex align-items-center justify-content-center"
-                                href="/donate/payment/"
-                            >
-                                Donate now
-                                <FaExternalLinkAlt className="ms-2"/>
-                            </Button>
-                        </div>
-                    </Container>
+                            <div className="text-center mt-4">
+                                <Button
+                                    variant="primary"
+                                    type="submit"
+                                    className="btn btn-lg btn-primary custom-btn w-100 d-flex align-items-center justify-content-center"
+                                    href="/donate/payment/"
+                                >
+                                    Donate now
+                                    <FaExternalLinkAlt className="ms-2"/>
+                                </Button>
+                            </div>
+                        </Container>
 
-                    <Container 
-                        className="py-4 px-4 custom-container"
-                    >
-                        <AchievementsCard/>
-                    </Container>
-                </Col>
-                <Col xs={12} lg={5} className="mt-lg-5 mb-lg-5 h-100 align-items-stretch">
+                        <Container 
+                            className="py-4 px-4 custom-container"
+                        >
+                            <AchievementsCard/>
+                        </Container>
+                    </Col>
+                </RoleOnly>
+
+                <Col 
+                    xs={12} 
+                    lg={user?.role === 'public' || user === null ? 5 : 12} 
+                    className={user?.role === 'public' || user === null ? "mt-lg-5 mb-lg-5 h-100" : "mt-5 mb-5 px-lg-5 h-100"}
+                    style={{ maxWidth: '1000px' }}
+                >
                     <Container 
                         className="py-4 text-center custom-container"
                     >
@@ -102,8 +113,8 @@ const Donations = () => {
                             <Container 
                                 style={{
                                     overflowY: 'auto',
-                                    minHeight: '95vh',
-                                    maxHeight: '95vh'
+                                    minHeight: '680px',
+                                    maxHeight: '680px'
                                 }} 
                                 className="py-4 px-4"
                             >
@@ -116,10 +127,18 @@ const Donations = () => {
                                         ))}
                                     </Row>
                                 ) : (
-                                    <div className="mt-4 d-flex justify-content-center">
-                                        <Alert variant="info" className="text-center">
-                                            Be our first donor.
-                                        </Alert>
+                                    <div className="d-flex justify-content-center">
+                                        <RoleOnly allowedRoles={['public', 'guest']}>
+                                            <Alert variant="info" className="text-center">
+                                                Be our first donor.
+                                            </Alert>
+                                        </RoleOnly>
+
+                                        <RoleOnly allowedRoles={['staff']}>
+                                            <Alert variant="info" className="text-center">
+                                                No donations.
+                                            </Alert>
+                                        </RoleOnly>
                                     </div>
                                 )}
 
